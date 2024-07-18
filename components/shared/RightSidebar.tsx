41b936f889f0d745c36478dce6c89ccb41388190
mesutkacmaz <mesut.kacmaz@outlook.com>
2024-07-18 16:34:06 +0300

    show top questions-popular tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,28 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
 
-const hotQuestions = [
-  { _id: "1", title: "How do I use express as a custom server in NextJS?" },
-  { _id: "2", title: "Cascading Deletes in SQLAlchemy?" },
-  { _id: "3", title: "How to perfectly center a div with TailwindCSS?" },
-  {
-    _id: "4",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-  },
-  { _id: "5", title: "Redux Toolkit Not Updating State as Expected" },
-];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
-const popularTags = [
-  { _id: "1", name: "JavaScript", totalQuestions: 5 },
-  { _id: "2", name: "React", totalQuestions: 5 },
-  { _id: "3", name: "Next.js", totalQuestions: 5 },
-  { _id: "4", name: "Vue", totalQuestions: 2 },
-  { _id: "5", name: "Redux", totalQuestions: 10 },
-];
-
-const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -31,7 +16,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">
@@ -56,7 +41,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
